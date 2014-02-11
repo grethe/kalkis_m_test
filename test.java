64f7f6803d939f230d1de60f9285434465844895
grethe <grethe.sandstrak@hist.no>
2014-02-11 12:59:38 +0100

    endringer i test.java

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 class test{
 	public static void main(String[] args){
 		System.out.println("Første versjon av main-metode i klassen test");
+		System.out.println("Andre versjon av main-metode i klassen test, lagt inn av grethes");
+
 	}
 }
